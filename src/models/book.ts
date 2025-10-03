4457fe4915c5562385132d89c3520528d1033b54
Maksim0403 <maksim.didychuk4@gmail.com>
2025-10-03 20:41:40 +0300

    feat: add Husky with pre-commit hooks

--- src/models/book.ts
+++ src/models/book.ts
@@ -1,25 +1,37 @@
 export interface IBook {
-    id: string;
-    title: string;
-    author: string;
-    year: number;
-    isBorrowed: boolean;
-    borrowedBy?: number;
+  id: string;
+  title: string;
+  author: string;
+  year: number;
+  isBorrowed: boolean;
+  borrowedBy?: number;
 }
 
 export class Book implements IBook {
-    constructor(
-        public id: string,
-        public title: string,
-        public author: string,
-        public year: number,
-        public isBorrowed: boolean = false,
-        public borrowedBy?: number
-    ) {}
+  constructor(
+    public id: string,
+    public title: string,
+    public author: string,
+    public year: number,
+    public isBorrowed: boolean = false,
+    public borrowedBy?: number,
+  ) {}
 
-    getTitle() { return this.title; }
-    getAuthor() { return this.author; }
-    getYear() { return this.year; }
-    borrow(userId: number) { this.isBorrowed = true; this.borrowedBy = userId; }
-    returned() { this.isBorrowed = false; this.borrowedBy = undefined; }
+  getTitle() {
+    return this.title;
+  }
+  getAuthor() {
+    return this.author;
+  }
+  getYear() {
+    return this.year;
+  }
+  borrow(userId: number) {
+    this.isBorrowed = true;
+    this.borrowedBy = userId;
+  }
+  returned() {
+    this.isBorrowed = false;
+    this.borrowedBy = undefined;
+  }
 }
