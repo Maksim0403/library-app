1bda6a1d06852fcab86892a4262bef75ef8980a5
Maksim0403 <maksim.didychuk4@gmail.com>
2025-10-07 11:48:05 +0300

    feat: fix bug with localStorage initialization

--- src/tests/libraryService.test.ts
+++ src/tests/libraryService.test.ts
@@ -3,6 +3,38 @@ import { LibraryService } from '../services/libraryService.js';
 import { Book } from '../models/book.js';
 import { User } from '../models/user.js';
 
+const createLocalStorageMock = (): Storage => {
+    let store: Record<string, string> = {};
+
+    return {
+        getItem: (key: string): string | null => {
+            return store[key] || null;
+        },
+        setItem: (key: string, value: string): void => {
+            store[key] = value.toString();
+        },
+        removeItem: (key: string): void => {
+            delete store[key];
+        },
+        clear: (): void => {
+            store = {};
+        },
+        get length(): number {
+            return Object.keys(store).length;
+        },
+        key: (index: number): string | null => {
+            const keys = Object.keys(store);
+            return keys[index] || null;
+        }
+    };
+};
+
+Object.defineProperty(global, 'localStorage', {
+    value: createLocalStorageMock(),
+    writable: true,
+    configurable: true
+});
+
 describe('LibraryService Tests', () => {
     let service: LibraryService;
 
